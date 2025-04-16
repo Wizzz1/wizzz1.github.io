ae318bd6a6f3e57be9245c7c4a500232799272d7
Wizzz1 <126481855+Wizzz1@users.noreply.github.com>
2025-04-17 00:59:55 +0800

    DOM declaration in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 // Main JavaScript file for the weather app
+let cityInput = document.getElementById("cityInput");
+let city = document.getElementById("city");
+let temperature = document.getElementById("temperature");
+let description = document.querySelector(".description");
+let clouds = document.getElementById("clouds");
+let humidity = document.getElementById("humidity");
+let wind = document.getElementById("windSpeed");
+let pressure = document.getElementById("pressure");
+let visibility = document.getElementById("visibility");
+let sunrise = document.getElementById("sunrise");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); /* prevent the web refresh and from submitting */
+  alert("Hello world.");
+});
