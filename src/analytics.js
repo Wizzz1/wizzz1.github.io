cced36e214abe25287985cbd1f6871720f0cce04
Wizzz1 <126481855+Wizzz1@users.noreply.github.com>
2025-04-20 15:21:31 +0800

    Add Google tag manager

--- src/analytics.js
+++ src/analytics.js
@@ -1,8 +1,8 @@
 // JavaScript file for analytics implementation
-window.dataLayer = window.dataLayer || []
+window.dataLayer = window.dataLayer || [];
 function gtag() {
-  dataLayer.push(arguments)
+  dataLayer.push(arguments);
 }
-gtag("js", new Date())
+gtag("js", new Date());
 
-gtag("config", "G-FRN92HFJW6")
+gtag("config", "G-FRN92HFJW6");
